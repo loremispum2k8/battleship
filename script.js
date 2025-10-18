7483d62b5d5949cb06eac71474092505fedea837
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-10-18 22:03:18 +0300

    Limit first 2 boat placements

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ class Ship {
     this.isSinked = false;
     this.coordonates = [];
     this.hits = [];
+    this.allowedMoves = [];
   }
 
   hit() {
@@ -22,18 +23,17 @@ class Ship {
     }
   }
 }
-//let carrier = new Ship(5)
-//let battleship = new Ship(4)
-//let cruiser = new Ship(3)
-//let submarine = new Ship(3)
-//let destroyer = new Ship(2)
-//console.log(carrier)
-//console.log(destroyer)
+
+
+
+
+
+
 
 class Gameboard {
   constructor() {
     this.criticalShots = [];
-    this.nonCriticalShots = [];
+    this.nonCriticalShots = {};
     this.historyShots = [];
     this.ships = {
       carrier: new Ship(5, "carrier"),
@@ -45,55 +45,146 @@ class Gameboard {
 
     let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
     for (let i = 1; i <= 10; i++) {
+      this.nonCriticalShots[`Row${i}`] = [];
       for (let letter of letters) {
-        this.nonCriticalShots.push([letter, String(i)]);
+        this.nonCriticalShots[`Row${i}`].push([letter, String(i)]);
       }
     }
   }
 
   placeBoat(ship, coordonateStr) {
     let historyShotsPass = true;
+    let allowedMovePass = false;
 
     let coordonate = [];
     coordonate.push(coordonateStr[0]);
     coordonate.push(coordonateStr.slice(1));
     console.log(coordonate);
 
-    for( let position of this.historyShots ){
-      if(position[0] === coordonate[0] && position[1] === coordonate[1]){
-        historyShotsPass = false;
-        console.log('Repeating position found')
-      }
-    } 
-    console.log(historyShotsPass)
-    
-    for (let element in this.ships) {
-      if (
-        ship === element &&
-        this.ships[element].coordonates.length < this.ships[element].length 
-        && historyShotsPass === true
-      ) {
-        console.log('passed')
-        this.ships[element].coordonates.push(coordonate);
-        this.criticalShots.push(coordonate);
-        this.historyShots.push(coordonate)
+    // CONSTRUCTING POSSIBLE MOVES
+    if (this.ships[ship].coordonates.length === 0) {
+      this.ships[ship].allowedMoves.push(coordonate);
+      console.log(this.ships[ship].allowedMoves);
+    }
+    if (this.ships[ship].coordonates.length === 1) {
+    }
+    // CONSTRUCTING POSSIBLE MOVES
 
-        for (let position of this.nonCriticalShots) {
-          if (position[0] === coordonate[0] && position[1] === coordonate[1]) {
-            this.nonCriticalShots.splice(
-              this.nonCriticalShots.indexOf(position),
-              1
-            );
+    // Check for repeating coordonate - SEEMS GOOD
+    for (let position of this.historyShots) {
+      if (position[0] === coordonate[0] && position[1] === coordonate[1]) {
+        historyShotsPass = false;
+        console.log("Repeating position found");
+      }
+    }
+    // Check for repeating coordonate - SEEMS GOOD
+
+    // Check for possibleMove
+    for (let possibleMove of this.ships[ship].allowedMoves) {
+      if (
+        possibleMove[0] === coordonate[0] &&
+        possibleMove[1] === coordonate[1]
+      ) {
+        allowedMovePass = true;
+      }
+    }
+    // Check for possibleMove
+
+    if (!allowedMovePass) {
+      console.log("Unnable to validate position");
+    }
+
+    // Add coordonate
+    if (
+      this.ships[ship].coordonates.length < this.ships[ship].length &&
+      historyShotsPass === true &&
+      allowedMovePass === true
+    ){
+
+      console.log("passed");
+      this.ships[ship].coordonates.push(coordonate);
+      this.criticalShots.push(coordonate);
+      this.historyShots.push(coordonate);
+
+      for (let row in this.nonCriticalShots) {
+        for (let position of this.nonCriticalShots[row]) {
+          for (let possibleMove of this.ships[ship].allowedMoves) {
+            if (
+              position[0] === coordonate[0] &&
+              position[1] === coordonate[1] &&
+              possibleMove[0] === coordonate[0] &&
+              possibleMove[1] === coordonate[1]
+            ) {
+              this.nonCriticalShots[row].splice(
+                this.nonCriticalShots[row].indexOf(position),
+                1
+              );
+            }
           }
         }
       }
+
+      console.log("Adding 4 more possitions");
+      if (this.ships[ship].coordonates.length === 1) {
+        let letter = this.ships[ship].coordonates[0][0];
+        let num = this.ships[ship].coordonates[0][1];
+
+        // [ L+1 ; N ]
+        if (letter.charCodeAt(0) + 1 >= 72) {
+          this.ships[ship].allowedMoves[1] = ["J", num];
+        } else {
+          this.ships[ship].allowedMoves[1] = [
+            String.fromCharCode(letter.charCodeAt(0) + 1),
+            num,
+          ];
+        }
+
+        // [ L-1 ; N ]
+        if (letter.charCodeAt(0) - 1 <= 65) {
+          this.ships[ship].allowedMoves[2] = ["A", num];
+        } else {
+          this.ships[ship].allowedMoves[2] = [
+            String.fromCharCode(letter.charCodeAt(0) - 1),
+            num,
+          ];
+        }
+
+        // [ L ; N + 1 ]
+        if (Number(num) + 1 >= 10) {
+          this.ships[ship].allowedMoves[3] = [letter, "10"];
+        } else {
+          this.ships[ship].allowedMoves[3] = [
+            letter,
+            String(Number(num) + 1),
+          ];
+        }
+
+        // [ L-1 ; N - 1 ]
+        if (Number(num) - 1 <= 0) {
+          this.ships[ship].allowedMoves[4] = [letter, "0"];
+        } else {
+          this.ships[ship].allowedMoves[4] = [
+            letter,
+            String(Number(num) - 1),
+          ];
+        }
+
+        this.ships[ship].allowedMoves = this.ships[ship].allowedMoves.filter(
+          (move) => move !== undefined
+        );
+        this.ships[ship].allowedMoves = this.ships[ship].allowedMoves.filter(
+          (move) => move.join("") !== coordonate.join("")
+        );
+        console.log(this.ships[ship].allowedMoves);
+      }
     }
-    console.log(this)
-  }
-  recieveAttack(x, y) {
-    //sends hit to the ship or the missing shot
+    // Addcoordonate
+    console.log(this);
   }
 }
+
+
+
 
 class Player {
   constructor() {
@@ -103,19 +194,20 @@ class Player {
 let me = new Player();
 let computer = new Player();
 
+
+
+
+
 // TEST SITE
+me.gameboard.placeBoat("submarine", "C6");
+me.gameboard.placeBoat("submarine", "C7");
 
-me.gameboard.placeBoat("submarine", "A10");
-me.gameboard.placeBoat("submarine", "A9");
-me.gameboard.placeBoat("submarine", "A8");
-me.gameboard.placeBoat("submarine", "A7");
-me.gameboard.placeBoat("submarine", "A8");
-me.gameboard.placeBoat("cruiser", "J5");
-me.gameboard.placeBoat("cruiser", "J5");
-console.log(me.gameboard);
 
+
+
+
+// DOM
 let squares = document.querySelectorAll(".square");
-
 squares.forEach((square) => {
   square.addEventListener("click", (e) => {
     for (let coordonate of me.gameboard.criticalShots) {
@@ -149,25 +241,33 @@ squares.forEach((square) => {
         }
       }
     }
-    
-    for (let coordonate of me.gameboard.nonCriticalShots) {
-      if (e.target.id === coordonate.join("")) {
-        console.log("Miss: ", coordonate);
-        square.style.backgroundColor = "white";
-        me.gameboard.nonCriticalShots.splice(me.gameboard.nonCriticalShots.indexOf(coordonate),1)
+
+    for (let row in me.gameboard.nonCriticalShots) {
+      for (let coordonate of me.gameboard.nonCriticalShots[row]) {
+        if (e.target.id === coordonate.join("")) {
+          console.log("Miss: ", coordonate);
+          square.style.backgroundColor = "white";
+          me.gameboard.nonCriticalShots[row].splice(
+            me.gameboard.nonCriticalShots[row].indexOf(coordonate),
+            1
+          );
+        }
       }
     }
 
     for (let ship in me.gameboard.ships) {
       for (let coordonate of me.gameboard.ships[ship].hits) {
-        if (coordonate.join("") === e.target.id && me.gameboard.ships[ship].isSinked) {
-            for(let hit of me.gameboard.ships[ship].hits){
-                squares.forEach(insideSquare =>{
-                    if(insideSquare.id === hit.join('')){
-                        insideSquare.style.backgroundColor = 'red'
-                    }
-                })
-            }
+        if (
+          coordonate.join("") === e.target.id &&
+          me.gameboard.ships[ship].isSinked
+        ) {
+          for (let hit of me.gameboard.ships[ship].hits) {
+            squares.forEach((insideSquare) => {
+              if (insideSquare.id === hit.join("")) {
+                insideSquare.style.backgroundColor = "red";
+              }
+            });
+          }
         }
       }
     }
